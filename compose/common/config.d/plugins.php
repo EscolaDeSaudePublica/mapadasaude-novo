ed66cbb9d2e686e6a8f3fd4a9b4d686a2f6e934e
Junior-Shyko <franciscoanto@gmail.com>
2022-01-26 13:28:09 -0300

    Adicionado plugin PDF, SealCertified, EvaluationMethodHomolog, EvaluationMethodTechnicalNa

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -5,9 +5,20 @@ return [
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
+        'EvaluationMethodHomolog' => ['namespace' => 'EvaluationMethodHomolog'],
+        'EvaluationMethodTechnicalNa' => ['namespace' => 'EvaluationMethodTechnicalNa'],
         
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
-        'LocationStateCity' => ['namespace' => 'LocationStateCity']
+        'LocationStateCity' => ['namespace' => 'LocationStateCity'],
+        'PDFReport' => ['namespace' => 'PDFReport'],
+        'SealModelTab' => ['namespace' => 'SealModelTab' ],
+        'SealCertified' => [ 
+            'namespace' => 'SealCertified',
+            'config' => [ 
+                'logo-site' => 'img/logo-saude.png' 
+            ] 
+        ],
+
     ]
 ];
